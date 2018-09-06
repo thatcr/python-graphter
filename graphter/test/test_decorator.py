90c8d13aa3e450833c83e5c6b18f686b07e659e6
Rob Thatcher <thatcr@users.noreply.github.com>
2018-09-06 22:54:20 +0100

    fix coverage

--- graphter/test/test_decorator.py
+++ graphter/test/test_decorator.py
@@ -2,15 +2,12 @@ from graphter import graph
 from inspect import signature
 
 def test_identical_signature():
-	def funk(a, b):
-		return a + b
+	def funk(a, b): pass
 	assert signature(funk) == signature(graph(funk))
 
 def test_ref():
 	@graph
-	def funk(a, b):
-		return locals()
-	
+	def funk(a, b): pass
 	assert tuple(funk.ref(1, 2)) == (funk, 1, 2)
 	
 	
